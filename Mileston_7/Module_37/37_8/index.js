62ac49a6c31660621776e288450c01b7c338c346
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-03 22:25:29 +0600

    37-8 dot vs bracket notation

---
+++ Mileston_7/Module_37/37_8/index.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Maruf',
+    age: 25,
+    10: 'secrate code',
+    "co-de" : 'jio'
+}
+// Dot notation 
+const personName = person.name
+console.log(personName)
+
+//Bracket noration
+const secrateKey = person[10]
+console.log(secrateKey)
+
+const code = person['co-de']
+console.log(code)
