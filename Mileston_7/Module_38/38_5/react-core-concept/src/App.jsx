c9fb5863f634808fb12ef4a8dd75fb1be4dc4801
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-04 12:51:39 +0600

    38-7 Six ways to do Conditional Rendering: If, Ternary and AND OR

--- Mileston_7/Module_38/38_5/react-core-concept/src/App.jsx
+++ Mileston_7/Module_38/38_5/react-core-concept/src/App.jsx
@@ -1,5 +1,26 @@
 
-// 38_6
+// // 38_6
+// import './App.css'
+// import ToDo from './ToDo';
+
+// function App() {
+//   const time = 50
+//   return (
+//     <>
+//       <div>
+//         <ToDo task='learn React' isDone={true} time={time} ></ToDo>
+//         <ToDo task='Revise Js' isDone={false} ></ToDo>
+//         <ToDo task='learn Php' isDone={true} time='100' ></ToDo>
+//       </div>
+//     </>
+//   )
+// }
+
+
+
+// export default App
+
+// 38_7
 import './App.css'
 import ToDo from './ToDo';
 
--- Mileston_7/Module_38/38_5/react-core-concept/src/ToDo.jsx
+++ Mileston_7/Module_38/38_5/react-core-concept/src/ToDo.jsx
@@ -1,7 +1,24 @@
+// export default function ToDo({ task, isDone, time = 0 }) {
+//         if(isDone){
+//             return(
+//                 <li>Task: {task} Duration: {time}</li>
+//             )
+//         } return <li>To be done: {task}</li>
+//     }
+
+
+//38_7   
 export default function ToDo({ task, isDone, time = 0 }) {
-        if(isDone){
-            return(
-                <li>Task: {task} Duration: {time}</li>
-            )
-        } return <li>To be done: {task}</li>
+    // return isDone? <li>Task: {task} time: {time}</li> : <li>Not done: {task} time= {time}</li>
+    // return isDone && <li>Task: {task} time: {time}</li>
+    // return isDone || <li>Task: {task} time: {time}</li>
+
+    let finalStatement;
+    if (isDone) {
+        finalStatement = <li>Task: {task} time: {time}</li>
+    }else{
+
+        finalStatement = <li>Not done: {task} time= {time}</li>
     }
+    return finalStatement;
+}
