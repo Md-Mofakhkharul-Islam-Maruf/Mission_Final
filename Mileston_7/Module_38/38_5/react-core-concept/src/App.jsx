7bd7e10e701a079ac4bfcdae82b6a176b2b8487d
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-04 14:54:41 +0600

    38-7 Six ways to do Conditional Rendering: If, Ternary and AND OR

--- Mileston_7/Module_38/38_5/react-core-concept/src/App.jsx
+++ Mileston_7/Module_38/38_5/react-core-concept/src/App.jsx
@@ -20,19 +20,50 @@
 
 // export default App
 
-// 38_7
+// // 38_7
+// import './App.css'
+// import ToDo from './ToDo';
+
+// function App() {
+//   const time = 50
+//   return (
+//     <>
+//       <div>
+//         <ToDo task='learn React' isDone={true} time={time} ></ToDo>
+//         <ToDo task='Revise Js' isDone={false} ></ToDo>
+//         <ToDo task='learn Php' isDone={true} time='100' ></ToDo>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+// 38_8
+import Actor from './Actor';
 import './App.css'
-import ToDo from './ToDo';
+import Singer from './Singer';
 
 function App() {
-  const time = 50
+
+  const actors = ['bapppa', 'sunny', 'hena']
+
+  const singers = [
+    { id: 1, name: 'Monir', age: 60 },
+    { id: 2, name: 'naznin', age: 60 },
+    { id: 3, name: 'sabina', age: 55 }
+  ]
+
   return (
     <>
-      <div>
-        <ToDo task='learn React' isDone={true} time={time} ></ToDo>
-        <ToDo task='Revise Js' isDone={false} ></ToDo>
-        <ToDo task='learn Php' isDone={true} time='100' ></ToDo>
-      </div>
+    <h2>Name of Actor: </h2>
+      {
+        actors.map(actor => <Actor actor={actor}></Actor>)
+      }
+      <h2>Name of singers: </h2>
+      {
+        singers.map(singer=><Singer id={singer.id} singer={singer} ></Singer>)
+      }
     </>
   )
 }
