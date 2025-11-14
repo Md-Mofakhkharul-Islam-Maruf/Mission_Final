9a8c9c8eff7ae96d9dc3e21306df288c0df7c0ea
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-14 11:41:34 +0600

    40-4 Use style in react and 3 column layout

--- Mileston_7/Module_40/react-advance/src/components/country/Country.jsx
+++ Mileston_7/Module_40/react-advance/src/components/country/Country.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import './country.css'
 
 const Country = ({country}) => {
     
     console.log(country)
+    const handleVisited = ()=>{
+        console.log('visited')
+    }
     return (
-        <div>
+        <div className='country'>
             <h3>Name: {country.name.common}</h3>
             <img src={country?.flags?.png} alt="" />
             <h3>Populations: {country?.population}</h3>
             <h3>Region: {country?.region}</h3>
+            <button onClick={handleVisited}>Visited</button>
         </div>
     );
 };
