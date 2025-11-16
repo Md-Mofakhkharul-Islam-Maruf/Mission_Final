af485a2e4213510156eb0ef9b20171c688081b4c
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-16 19:07:49 +0600

    41-6 [optional] Introduction to Local Storage: Storing Data in the Browser

--- Mileston_7/Module_41/awesome-water-bottles/src/componentS/bottleS/Bottles.jsx
+++ Mileston_7/Module_41/awesome-water-bottles/src/componentS/bottleS/Bottles.jsx
@@ -3,15 +3,21 @@ import Bottle from '../bottlE/Bottle';
 import './bottles.css'
 
 const Bottles = ({ bottlesPromise }) => {
+
     const bottles = use(bottlesPromise)
     console.log(bottles.length)
+
     const [cart, setCart] = useState([])
+
     const handleaddCart = (bottle) => {
-        console.log('Added to cart succesfully :', bottle)
+        // console.log('Added to cart succesfully :', bottle)
+        const newCart = [...cart, bottle]
+        setCart(newCart)
     }
     return (
         <div>
             <h3>All bottles are: {bottles.length}</h3>
+            <h5>Added bottle: {cart.length}</h5>
             <div className='bottole-container'>
                 {
                     bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleaddCart={handleaddCart}></Bottle>)
