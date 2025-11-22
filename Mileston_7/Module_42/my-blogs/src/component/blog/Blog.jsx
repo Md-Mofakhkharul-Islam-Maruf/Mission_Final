062aced31c57089f5fb9878df3e05fbe743e3280
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-11-22 08:46:01 +0600

    style each card||| install react icon ||

--- Mileston_7/Module_42/my-blogs/src/component/blog/Blog.jsx
+++ Mileston_7/Module_42/my-blogs/src/component/blog/Blog.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { IoBookmarksSharp } from 'react-icons/io5';
 
 const Blog = ({ blog }) => {
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card bg-base-100 w-96 shadow-sm m-10">
             <figure>
                 <img
                     src={blog.cover}
                     alt="blog image" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>{blog.title}</p>
+                <div className='flex justify-around items-center'>
+                    <img className='w-12' src={blog.author_img} alt="" />
+                    <h3 className='text-2xl font-semibold'>{blog.author}</h3>
+                    <IoBookmarksSharp size={25} />
+                </div>
+                <p className='text-lg font-semibold'>{blog.title}</p>
+                <div className='flex text-2xl'>
+                    {
+                        blog.hashtags.map(hash => <p>{hash}</p>)
+                    }
+                </div>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Mark as Read</button>
                 </div>
