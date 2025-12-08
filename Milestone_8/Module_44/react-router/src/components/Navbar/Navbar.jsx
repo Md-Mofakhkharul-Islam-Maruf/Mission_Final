86fe686e2806feae94a2da3890b6eadf9f207b34
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-08 21:30:30 +0600

    change comment color in setting

---
+++ Milestone_8/Module_44/react-router/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+
+            {/* conventional static way to create a navbar */}
+            {/* <ul className='flex justify-center'>
+                <li className='mx-5'>Home</li>
+                <li className='mx-5'>About</li>
+                <li className='mx-5'>Contact</li>
+                <li className='mx-5'>Game</li>
+            </ul> */}
+        </nav>
+    );
+};
+
+export default Navbar;
