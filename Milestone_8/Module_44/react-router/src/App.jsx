771a07fa35da444af833f8d2715d04ec4a451f88
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-10 16:16:01 +0600

    rechart explore

--- Milestone_8/Module_44/react-router/src/App.jsx
+++ Milestone_8/Module_44/react-router/src/App.jsx
@@ -3,10 +3,34 @@ import './App.css'
 import DaisyNav from './components/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
 import PricingOption from './components/pricingOptions/PricingOption'
+import MarksCharts from './components/marksCharts/MarksCharts'
 
 const pricingPromise = fetch('pricingdata.json').then(res => res.json())
 
 function App() {
+  const marksdata = [
+    {
+      "name": "Alice",
+      "roll": "101",
+      "physics": 85,
+      "chemistry": 78,
+      "math": 92
+    },
+    {
+      "name": "Bob",
+      "roll": "102",
+      "physics": 74,
+      "chemistry": 81,
+      "math": 88
+    },
+    {
+      "name": "Charlie",
+      "roll": "103",
+      "physics": 90,
+      "chemistry": 87,
+      "math": 95
+    }
+  ]
 
   return (
     <>
@@ -17,6 +41,10 @@ function App() {
       <main>
         <Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
           <PricingOption pricingPromise={pricingPromise}></PricingOption>
+        </Suspense>
+
+        <Suspense fallback={<span className="loading loading-dots loading-lg"></span>}>
+          <MarksCharts marksdata={marksdata}></MarksCharts>
         </Suspense>
       </main>
       <footer></footer>
---
+++ Milestone_8/Module_44/react-router/src/components/marksCharts/MarksCharts.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
+
+const MarksCharts = ({marksdata}) => {
+
+    return (
+        <div>
+            <LineChart width={1000} height={500} data={marksdata}>
+                <XAxis dataKey={name}></XAxis>
+                <YAxis ></YAxis>
+                <Line  dataKey='physics' stroke='red'></Line>
+                <Line  dataKey='chemistry' stroke='blue'></Line>
+                <Line  dataKey='math' stroke='green'></Line>
+            </LineChart>
+        </div>
+    );
+};
+
+export default MarksCharts;
