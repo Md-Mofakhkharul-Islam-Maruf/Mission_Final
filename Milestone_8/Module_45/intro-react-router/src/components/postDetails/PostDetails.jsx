1928f1e36b46e3ec195cd6228fa76526e45e7bfa
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-11 23:34:42 +0600

    simple usage of useNavigate()

--- Milestone_8/Module_45/intro-react-router/src/components/postDetails/PostDetails.jsx
+++ Milestone_8/Module_45/intro-react-router/src/components/postDetails/PostDetails.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
+    const navigate = useNavigate()
     const details = useLoaderData()
     console.log(details)
     return (
         <div style={{ border: '2px solid blue', padding: '10px', borderRadius: '10px' }}>
             <p>{details.title}</p>
             <p>{details.body}</p>
+            <button onClick={() => navigate(`/users2`)}>Go to Users2 page</button>
         </div>
     );
 };
