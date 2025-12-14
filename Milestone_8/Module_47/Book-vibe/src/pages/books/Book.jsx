fa33f2dac3705ab480634fac6ddae57158e68641
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-14 16:37:04 +0600

    polish books card

--- Milestone_8/Module_47/Book-vibe/src/pages/books/Book.jsx
+++ Milestone_8/Module_47/Book-vibe/src/pages/books/Book.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import { FaStarHalf } from "react-icons/fa";
 
 const Book = ({ singleBook }) => {
     const {bookName, image, author, totalpages, rating, category, review} = singleBook
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
+        <div className="card bg-base-100 shadow-sm border">
+            <figure className='p-5 bg-gray-200'>
                 <img 
-                    className='w-2xl h-80'
+                    className=' h-42'
                     src={image}
                     alt={bookName} />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    Card Title
+                    {bookName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
                 <p>{author}s</p>
                 <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating}<FaStarHalf></FaStarHalf></div>
                 </div>
             </div>
         </div>
--- Milestone_8/Module_47/Book-vibe/src/pages/books/Books.jsx
+++ Milestone_8/Module_47/Book-vibe/src/pages/books/Books.jsx
@@ -11,7 +11,7 @@ const Books = () => {
     return (
         <div>
             <h1 className='text-4xl font-semibold text-center'>All Books are here.........</h1>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-20'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     allBooks.map(singleBook => <Book singleBook={singleBook} key={singleBook.bookId}></Book>)
                 }
