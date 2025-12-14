831061d381733ee7b8f30083ffb10e13b2d82a1d
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-14 08:48:01 +0600

    context api nice things

--- Milestone_8/Module_46/react-router-2nd/src/components/family/Cousine.jsx
+++ Milestone_8/Module_46/react-router-2nd/src/components/family/Cousine.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Special from './Special';
+import { AssetContext } from './FamilyTree';
 
 const Cousine = ({name}) => {
+    const asset = useContext(AssetContext)
     return (
         <div>
             {name}
+            <p>Asset: {asset}</p>
         </div>
     );
 };
--- Milestone_8/Module_46/react-router-2nd/src/components/family/FamilyTree.jsx
+++ Milestone_8/Module_46/react-router-2nd/src/components/family/FamilyTree.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Granpa from './Granpa';
 import './FamilyTree.css'
 
+export const AssetContext = createContext('')
+
 const FamilyTree = () => {
+    const asset = 'gold'
     return (
         <div className='family-tree'>
-            <Granpa></Granpa>
+            <h1>Family tree</h1>
+            <AssetContext.Provider value={asset}>
+                <Granpa></Granpa>
+            </AssetContext.Provider>
         </div>
     );
 };
