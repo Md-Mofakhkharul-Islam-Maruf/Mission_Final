c452da4d93764c8bc755ef4c6bdf4493ec2fbdba
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-15 15:27:43 +0600

    added bookid to localstorge to show mark as read,

--- Milestone_8/Module_47/Book-vibe/src/pages/bookDetails/BookDetails.jsx
+++ Milestone_8/Module_47/Book-vibe/src/pages/bookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoreDB } from '../../utilities/utilities';
 
 const BookDetails = () => {
     const { id } = useParams()
@@ -7,13 +8,17 @@ const BookDetails = () => {
     const data = useLoaderData()
 
     const singleBook = data.find(book => book.bookId === BookID)
-    const { bookId, bookName, image } = singleBook
+    const { bookId, bookName, image } = singleBook || {}
+
+    const handleMarkAsRead = (bookId) => {
+        addToStoreDB(bookId)
+    }
     return (
         <div>
             <img className='w-40' src={image} alt="" />
             <h2 className=''>{bookName}</h2>
             <div className='flex gap-5'>
-                <button className='btn btn-primary'>Read</button>
+                <button onClick={() => handleMarkAsRead(bookId)} className='btn btn-primary'>Mark as Read</button>
                 <button className='btn btn-secondary'>Wish List</button>
             </div>
         </div>
---
+++ Milestone_8/Module_47/Book-vibe/src/utilities/utilities.js
@@ -0,0 +1,27 @@
+const getStoredBook = () => {
+
+    const storedBookSTR = localStorage.getItem('readList')
+
+    if (storedBookSTR) {
+        const storedBookData = JSON.parse(storedBookSTR)
+        return storedBookData;
+    }
+    else {
+        return [];
+    }
+}
+
+const addToStoreDB = (bookId) => {
+
+    const storedBookData = getStoredBook()
+
+    if (storedBookData.includes(bookId)) {
+        console.log(bookId)
+        alert('ID to dekhi localStorage a ache vai')
+    } else {
+        storedBookData.push(bookId)
+        const data = JSON.stringify(storedBookData)
+        localStorage.setItem('readList', data)
+    }
+}
+export {addToStoreDB}
