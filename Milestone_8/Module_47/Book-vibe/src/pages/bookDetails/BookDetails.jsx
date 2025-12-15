63e4a9ba533d9797c03e152cd43a0383079904b3
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-12-15 22:48:42 +0600

    sweet alert and toastify install and host on surge and Netlify

--- Milestone_8/Module_47/Book-vibe/src/pages/bookDetails/BookDetails.jsx
+++ Milestone_8/Module_47/Book-vibe/src/pages/bookDetails/BookDetails.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
-import { addToStoreDB } from '../../utilities/utilities';
+import { addToStoreDB } from '../../utilities/utilities'; import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content';
+
+import { ToastContainer, toast } from 'react-toastify';
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
     const { id } = useParams()
@@ -11,12 +16,19 @@ const BookDetails = () => {
     const { bookId, bookName, image } = singleBook || {}
 
     const handleMarkAsRead = (bookId) => {
+        // Swal.fire({
+        //     title: "Drag me!",
+        //     icon: "success",
+        //     draggable: true
+        // });
+        toast("Wow so easy!")
         addToStoreDB(bookId)
     }
     return (
         <div>
             <img className='w-40' src={image} alt="" />
             <h2 className=''>{bookName}</h2>
+            <ToastContainer />
             <div className='flex gap-5'>
                 <button onClick={() => handleMarkAsRead(bookId)} className='btn btn-primary'>Mark as Read</button>
                 <button className='btn btn-secondary'>Wish List</button>
